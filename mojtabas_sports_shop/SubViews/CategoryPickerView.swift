a4b33f63d33db8cbac2ddf87a4771454c7cd80e2
Efen Stakes <efenstakes101@gmail.com>
2021-11-04 19:56:13 +0300

    add productr category picker view

--- mojtabas_sports_shop/SubViews/CategoryPickerView.swift
+++ mojtabas_sports_shop/SubViews/CategoryPickerView.swift
@@ -8,13 +8,34 @@
 import SwiftUI
 
 struct CategoryPickerView: View {
+    let category: ProductCategory
+    let isSelected: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+        VStack(spacing: 8) {
 
-struct CategoryPickerView_Previews: PreviewProvider {
-    static var previews: some View {
-        CategoryPickerView()
+            // icon
+            Image(category.getImage())
+                .resizable()
+                .frame(width: 30, height: 30, alignment: .center)
+
+            // dot to show when selected
+            Circle().fill(Color.pink)
+                .frame(width: 16, height: 16, alignment: .center)
+                .scaleEffect(
+                    isSelected ? 1 : 0
+                )
+                .animation(.spring())
+
+        }
+        .padding()
+        .frame(maxHeight: 110)
+        .background(
+            isSelected ? Color.pink.opacity(0.3) : Color.white
+        )
+        .cornerRadius(80)
+        .foregroundColor(.black)
+        .padding()
+        
     }
 }
