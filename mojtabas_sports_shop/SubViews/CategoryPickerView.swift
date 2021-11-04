2011f4ada228691469f5385e864b6956dd955c8c
Efen Stakes <efenstakes101@gmail.com>
2021-11-04 19:55:31 +0300

    add more images

---
+++ mojtabas_sports_shop/SubViews/CategoryPickerView.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryPickerView.swift
+//  mojtabas_sports_shop
+//
+//  Created by MAC on 11/4/21.
+//
+
+import SwiftUI
+
+struct CategoryPickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CategoryPickerView_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryPickerView()
+    }
+}
